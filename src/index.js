e28d4f5d95d889c8b4d5dc69d2de69746f4271db
Ivan Driuk <idriuk@zaraffasoft.com>
2019-03-05 16:57:29 +0200

    set from y for more beautiful deck animation after page reload

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const cards = [
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
-const from = i => ({ x: 0, y: i * -4, rot: 0, scale: 1.5/* , y: -1000 */ })
+const from = i => ({ x: 0, y:  -1000, rot: 0, scale: 1.5 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
